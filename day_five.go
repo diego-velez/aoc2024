0a592a20f59affc8bd19f7c7b678eb1834783ab3
Diego Vélez <diego.velez.dev@gmail.com>
2024-12-05 12:26:15 -0400

    Solve day 5 part 2

--- day_five.go
+++ day_five.go
@@ -11,8 +11,8 @@ type Rule struct {
 	After  int
 }
 
-func dayFiveResult() int {
-	var rules []Rule
+func dayFiveResult() (int, int) {
+	rules := make(map[int][]int)
 	var pages [][]int
 	scanningPages := false
 	ParseFile("resources/day_five_input", func(line string) {
@@ -31,12 +31,13 @@ func dayFiveResult() int {
 	return doIt(rules, pages)
 }
 
-func doIt(rules []Rule, pages [][]int) int {
+func doIt(rules map[int][]int, pages [][]int) (int, int) {
 	// Basicamente puedo buscar por cada page de atras pa lante, despues buscando ese page rule
 	// usando ese como el before, salvo todos los after en same array, despues voy chequiando
 	// si esta en array, si esta pues no follows the rules pq vino antes pero se supone que viniera
 	// despues
-	total := 0
+	totalCorrectInstructions := 0
+	totalIncorrectInstructions := 0
 
 	for _, instructions := range pages {
 		var allAfter []int
@@ -45,27 +46,66 @@ func doIt(rules []Rule, pages [][]int) int {
 		for i := len(instructions) - 1; i >= 0; i-- {
 			page := instructions[i]
 			if slices.Contains(allAfter, page) {
+				fixOrder(&instructions, i, rules)
 				validInstruction = false
-				break
+				i = len(instructions)
+				allAfter = []int{}
+			} else {
+				getAfters(page, rules, &allAfter)
 			}
-			getAfters(page, rules, &allAfter)
 		}
 
-		if !validInstruction {
-			continue
-		}
 		midIndex := (len(instructions) - 1) / 2
-		total += instructions[midIndex]
+		if validInstruction {
+			totalCorrectInstructions += instructions[midIndex]
+		} else {
+			totalIncorrectInstructions += instructions[midIndex]
+		}
 	}
 
-	return total
+	return totalCorrectInstructions, totalIncorrectInstructions
 }
 
-func getAfters(page int, rules []Rule, out *[]int) {
-	for _, rule := range rules {
-		if rule.Before == page {
-			*out = append(*out, rule.After)
+// fixOrder will swap pages that violate the rules, it will check what rule was broken and swap
+// them from instructions
+func fixOrder(instructions *[]int, i int, rules map[int][]int) {
+	page := (*instructions)[i]
+
+	// Get all before pages from which the problematic page is supposed to come after
+	var losBefores []int
+	for before, afters := range rules {
+		for _, after := range afters {
+			if page == after {
+				losBefores = append(losBefores, before)
+			}
 		}
+	}
+
+	// Get the before page that comes after the i page, causing the problem
+	elBeforeAfterIndex := -1
+	for _, before := range losBefores {
+		for j := i + 1; j < len(*instructions); j++ {
+			unBeforeAfter := (*instructions)[j]
+			if before == unBeforeAfter {
+				elBeforeAfterIndex = j
+				break
+			}
+		}
+
+		if elBeforeAfterIndex != -1 {
+			break
+		}
+	}
+
+	// Perform the swap
+	(*instructions)[i], (*instructions)[elBeforeAfterIndex] = (*instructions)[elBeforeAfterIndex], (*instructions)[i]
+}
+
+// getAfters will get all the pages that are supposed to come after page according to the rules
+// and append all of them to out
+func getAfters(page int, rules map[int][]int, out *[]int) {
+	for _, rule := range rules[page] {
+		*out = append(*out, rule)
 	}
 }
 
@@ -78,9 +118,14 @@ func parsePages(line string, out *[][]int) {
 	*out = append(*out, pages)
 }
 
-func parseRules(line string, out *[]Rule) {
+func parseRules(line string, out *map[int][]int) {
 	parseLine := strings.Split(line, "|")
 	num1, _ := strconv.Atoi(parseLine[0])
 	num2, _ := strconv.Atoi(parseLine[1])
-	*out = append(*out, Rule{Before: num1, After: num2})
+
+	if (*out)[num1] == nil {
+		(*out)[num1] = []int{}
+	}
+
+	(*out)[num1] = append((*out)[num1], num2)
 }
