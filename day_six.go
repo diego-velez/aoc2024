d9bc1c1565e692f707786fe6400e449d4d4fabb9
Diego Vélez <diego.velez.dev@gmail.com>
2024-12-06 08:53:52 -0400

    WIP: day six part 2

--- day_six.go
+++ day_six.go
@@ -1,5 +1,10 @@
 package main
 
+type Coord struct {
+	Row    int
+	Column int
+}
+
 type Direction int
 
 const (
@@ -9,7 +14,7 @@ const (
 	DOWN
 )
 
-func daySixResult() int {
+func daySixResult() (int, int) {
 	var path [][]rune
 	ParseFile("./resources/day_six_input", func(line string) {
 		var row []rune
@@ -18,12 +23,55 @@ func daySixResult() int {
 		}
 		path = append(path, row)
 	})
-	return distinctPositions(path)
+
+	distinctPositions, pathVisualization := distinctPositions(path, nil)
+	return distinctPositions, calcPossibleObstructions(pathVisualization)
 }
 
-func distinctPositions(path [][]rune) int {
+func calcPossibleObstructions(path [][]rune) int {
+	return 0
+}
+
+func getPossibleObstructionPlaces(path [][]rune) []Coord {
+	// TODO: should probably use a set for the possibleelocations
+	for _, row := range path {
+		for _, char := range row {
+			if char == '#' {
+			}
+		}
+	}
+	return []Coord{}
+}
+
+func getPossibleObstructionsForObsticle(path [][]rune, rowIndex, columnIndex int) []Coord {
+	var possibleLocations []Coord
+
+	for thisRowIndex, row := range path {
+		for thisColumnIndex, char := range row {
+			// Check quadrant 1, row + 1 coming from top
+			// Check quadrant 2, column + 1 coming from left
+			// check quadrant 3, row - 1 coming from bottom
+			// Check quadrant 4, column - 1 coming from right
+			quadrant1 := thisRowIndex == rowIndex+1 && char == '|'
+			quadrant2 := thisColumnIndex == columnIndex+1 && char == '-'
+			quadrant3 := thisRowIndex == rowIndex-1 && char == '|'
+			quadrant4 := thisColumnIndex == columnIndex-1 && char == '-'
+			if quadrant1 || quadrant2 || quadrant3 || quadrant4 {
+				possibleLocations = append(possibleLocations, Coord{Row: thisRowIndex, Column: thisColumnIndex})
+			}
+		}
+	}
+
+	return possibleLocations
+}
+
+func distinctPositions(path [][]rune, pathVis [][]rune) (int, [][]rune) {
 	// NOTE: You can visualize here
-	// printMap(path)
+	// printMap(pathVis)
+	if pathVis == nil {
+		pathVis = deepCopyMap(path)
+	}
+
 	rowIndex, columnIndex := currentPosition(path)
 	direction, nextDirectionChar := heading(path[rowIndex][columnIndex])
 
@@ -43,20 +91,29 @@ func distinctPositions(path [][]rune) int {
 
 		outOfBounds := frontRowIndex < 0 || frontRowIndex >= len(path) || frontColumnIndex < 0 || frontColumnIndex >= len(path[0])
 		if outOfBounds {
-			return calcDistinctPositions(path)
+			return calcDistinctPositions(path), pathVis
 		}
 
 		blocked = path[frontRowIndex][frontColumnIndex] == '#'
 		if !blocked {
 			path[frontRowIndex][frontColumnIndex] = path[rowIndex][columnIndex]
+
 			path[rowIndex][columnIndex] = 'X'
+
+			if direction == UP || direction == DOWN {
+				pathVis[frontRowIndex][frontColumnIndex] = '|'
+			} else {
+				pathVis[frontRowIndex][frontColumnIndex] = '-'
+			}
+
 			rowIndex, columnIndex = frontRowIndex, frontColumnIndex
 		}
 	}
 
 	path[rowIndex][columnIndex] = nextDirectionChar
+	pathVis[rowIndex][columnIndex] = '+'
 
-	return distinctPositions(path)
+	return distinctPositions(path, pathVis)
 }
 
 func calcDistinctPositions(path [][]rune) int {
@@ -104,4 +161,15 @@ func printMap(path [][]rune) {
 		println()
 	}
 	println()
+}
+
+func deepCopyMap(path [][]rune) [][]rune {
+	pathCopy := make([][]rune, len(path))
+
+	for i, row := range path {
+		pathCopy[i] = make([]rune, len(row))
+		copy(pathCopy[i], row)
+	}
+
+	return pathCopy
 }
